a404a453fa5be9a3ff8ee368e39882dba55662a6
Avvi Singh <singh.avvi98@gmail.com>
2020-11-02 12:47:16 +1100

    Adding the js file which contains the question-set stored in an array

---
+++ quiz.js
@@ -0,0 +1,40 @@
+const Questions = [{
+    title: 'Question 1',
+    question: 'Q1 This is a sample question',
+    correctOption: 'Q1 This is the correct answer',
+    option1: 'Q1 This is 1 incorrect option',
+    option2: 'Q1 This is incorrect option #2',
+    option3: 'Q1 This is incorrect option #3'
+},
+{
+    title: 'Question 2',
+    question: 'Q2 This is a sample question',
+    correctOption: 'Q2 This is the correct answer',
+    option1: 'Q2 This is 1 incorrect option',
+    option2: 'Q2 This is incorrect option #2',
+    option3: 'Q2 This is incorrect option #3'
+},
+{
+    title: 'Question 3',
+    question: 'Q3 This is a sample question',
+    correctOption: 'Q3 This is the correct answer',
+    option1: 'Q3 This is 1 incorrect option',
+    option2: 'Q3 This is incorrect option #2',
+    option3: 'Q3 This is incorrect option #3'
+},
+{
+    title: 'Question 4',
+    question: 'Q4 This is a sample question',
+    correctOption: 'Q4 This is the correct answer',
+    option1: 'Q4 This is 1 incorrect option',
+    option2: 'Q4 This is incorrect option #2',
+    option3: 'Q4 This is incorrect option #3'
+},
+{
+    title: 'Question 5',
+    question: 'Q5 This is a sample question',
+    correctOption: 'Q5 This is the correct answer',
+    option1: 'Q5 This is 1 incorrect option',
+    option2: 'Q5 This is incorrect option #2',
+    option3: 'Q5 This is incorrect option #3'
+}];
